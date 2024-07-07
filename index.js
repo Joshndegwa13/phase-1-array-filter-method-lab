a6ab11e52a30d7203d8f7656edde448cb3a9c5a1
Joshndegwa13 <ndegwajosh7@gmail.com>
2024-07-07 22:08:54 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// index.js
+
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver.startsWith(string));
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string);
+}
+
+module.exports = { findMatching, fuzzyMatch, matchName };
